bf052bab74907a1a4cbf0fbc6495cff2ac028ef8
nguyenphuongvn24220@gmail.com <nguyenphuongvn24220@gmail.com>
2024-09-04 21:03:49 +0200

    spring v1.0

---
+++ src/test/java/com/springboot/demo/my_app/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.demo.my_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
