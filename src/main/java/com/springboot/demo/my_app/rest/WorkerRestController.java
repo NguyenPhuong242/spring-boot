e612369506569cdf3520fe8351405fe291ead8d5
nguyenphuongvn24220@gmail.com <nguyenphuongvn24220@gmail.com>
2024-09-05 21:10:41 +0200

    app v1.0

--- src/main/java/com/springboot/demo/my_app/rest/WorkerRestController.java
+++ src/main/java/com/springboot/demo/my_app/rest/WorkerRestController.java
@@ -1,6 +1,7 @@
 package com.springboot.demo.my_app.rest;
 
 import com.springboot.demo.my_app.entity.Worker;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,31 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class WorkerRestController {
+
+    private List<Worker> workers;
+    // define @PostConstruct to load the worker data only once
+
+    @PostConstruct
+    public void loadData() {
+        workers = new ArrayList<>();
+        workers.add(new Worker("John", 30));
+        workers.add(new Worker("Mike", 25));
+        workers.add(new Worker("Tom", 35));
+    }
+
     // define endpoint for "/workers"
     // return list of workers
     // http://localhost:1208/my-app/api/workers
     @GetMapping("/workers")
     public List<Worker> getWorkers() {
-        List<Worker> workers = new ArrayList<>();
-        workers.add(new Worker("John", 30));
-        workers.add(new Worker("Mike", 25));
-        workers.add(new Worker("Tom", 35));
         return workers;
+    }
+
+    // define endpoint for "/workers/workerId"
+    // return worker at index
+    // http://localhost:1208/my-app/api/workers/1
+    @GetMapping("/workers/{workerId}")
+    public Worker getWorker(int workerId) {
+        return workers.get(workerId);
     }
 }
